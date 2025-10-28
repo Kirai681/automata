2afc69a49f20356b233d850a576c787c4b095870
Daniel <daniel.allerborn@rhenus.com>
2025-10-28 10:02:47 +0100

    add a cursor

--- main.py
+++ main.py
@@ -16,6 +16,9 @@ def main():
     all_items = pygame.sprite.Group()
     all_components = pygame.sprite.Group()
 
+    cursor_x = 0
+    cursor_y = 0
+
     running = True
     while running:
         for event in pygame.event.get():
@@ -26,11 +29,21 @@ def main():
                 if event.key == pygame.K_ESCAPE and not state.is_paused():
                     state.pause()
                     print("pause")
+                elif event.key == pygame.K_SPACE and not state.is_paused():
+                    state.toggle_simulation()
                 elif event.key == pygame.K_ESCAPE and state.is_paused():
                     state.resume()
                     print("resume")
-                if event.key == pygame.K_SPACE and not state.is_paused():
-                    state.toggle_simulation()
+
+                if state.is_build():
+                    if event.key == pygame.K_w and cursor_y > 0:
+                        cursor_y -= 1
+                    if event.key == pygame.K_s and cursor_y < c.GRID_SIZE - 1:
+                        cursor_y += 1
+                    if event.key == pygame.K_a and cursor_x > 0:
+                        cursor_x -= 1
+                    if event.key == pygame.K_d and cursor_x < c.GRID_SIZE - 1:
+                        cursor_x += 1
 
         screen.fill(c.BACKGORUND_COLOR)
 
@@ -44,6 +57,14 @@ def main():
                         c.CELL_SIZE,
                     )
                     pygame.draw.rect(screen, c.GRID_COLOR, rect, 1)
+
+            cursor_rect = pygame.Rect(
+                cursor_x * c.CELL_SIZE,
+                cursor_y * c.CELL_SIZE,
+                c.CELL_SIZE,
+                c.CELL_SIZE,
+            )
+            pygame.draw.rect(screen, c.HIGHLIGHT_COLOR, cursor_rect, 3)
 
         clock.tick(c.TARGET_FPS)
 
--- src/component.py
+++ src/component.py
@@ -14,7 +14,10 @@ class Component(Sprite):
             self.image = pygame.Surface((c.CELL_SIZE, c.CELL_SIZE))
             self.image.fill((200, 200, 200))
         else:
-            self.image = pygame.transform.scale(image, (c.CELL_SIZE, c.CELL_SIZE))
+            self.image = pygame.transform.scale(
+                image,
+                (c.CELL_SIZE, c.CELL_SIZE),
+            )
 
         self.rect = self.image.get_rect()
         self.rect.topleft = (x * c.CELL_SIZE, y * c.CELL_SIZE)
